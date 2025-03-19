6242cf1e9b451f8c096deb5d61cee44d9815322e
PeterHenryy <snakenoxz@gmail.com>
2025-03-19 16:39:17 -0300

    make it better

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -1,4 +1,6 @@
-﻿// Please see documentation at https://learn.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
+﻿const burgerMenu = document.getElementById('burger-menu');
+const navbar = document.getElementById('navbar');
 
-// Write your JavaScript code.
+burgerMenu.addEventListener('click', () => {
+    navbar.classList.toggle('active');
+});
